611d1fb12d6acca58ae8b6cc4c8ccc9ef8b88466
Joakim Loxdal <loxdal@kth.se>
2019-01-19 18:40:34 +0100

    Added church creating crusadors like crazy

--- bots/our-amazing-bot/church.js
+++ bots/our-amazing-bot/church.js
@@ -1,8 +1,17 @@
 import structureHelper from './structure.js';
+import {BCAbstractRobot, SPECS} from 'battlecode';
 
 var churchHelper = {
   turn: self => {
     // we do stuff
+    if (self.fuel >= 50) {
+      let location = {x: self.me.x, y: self.me.y};
+      let possibleDirections = structureHelper.getPossibleDirections(location, self.map, self.getVisibleRobotMap())
+      let randomDirection = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
+
+      self.log('Church building a crusader at ' + (self.me.x+randomDirection.x) + ',' + (self.me.y+randomDirection.y));
+      return self.buildUnit(SPECS.CRUSADER, randomDirection.x, randomDirection.y);
+    }
 
     return null;
   }
