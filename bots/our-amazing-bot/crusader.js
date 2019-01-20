ee303f3e98e9b509c59642c809e919d057831f9d
Joakim Loxdal <loxdal@kth.se>
2019-01-20 23:41:28 +0100

    Crusaders are fine now

--- bots/our-amazing-bot/crusader.js
+++ bots/our-amazing-bot/crusader.js
@@ -1,13 +1,16 @@
 import unitHelper from './unit.js';
+import nav from './nav.js';
 import {BCAbstractRobot, SPECS} from 'battlecode';
 
 var crusaderHelper = {
   turn: self => {
     // On the first turn, find out our base
     if (!self.castle) {
+      // Set castle as home if castle
         self.castle = self.getVisibleRobots()
         .filter(robot => robot.team === self.me.team && robot.unit === SPECS.CASTLE)[0];
         if(!self.castle){
+          // Set church as home if church
           self.castle = self.getVisibleRobots()
           .filter(robot => robot.team === self.me.team && robot.unit === SPECS.CHURCH)[0];
         }
@@ -19,6 +22,7 @@ var crusaderHelper = {
       }else{
         self.task = "attack_opponent";
       }
+      self.task = "attack_opponent";
     }
 
     // Current positions
@@ -32,9 +36,24 @@ var crusaderHelper = {
 
     // Set new destination if none exist
     if(!self.destination || !self.distanceMap){
+      self.mapIsHorizontal = 1;
+      if(nav.isVertical(self.map)){
+        self.mapIsHorizontal = 0;
+        self.log("Map is vertically mirrored");
+      }else{
+        self.log("Map is horizontally mirrored");
+      }
+      self.spawnPoint = location;
+      self.mirroredSpawn = unitHelper.reflect(self.me, self.getPassableMap(), self.mapIsHorizontal);
+
+      // Init positions
+      let interestingLocations = [];
+
       if(self.task === "attack_opponent"){
-        self.log("Adding mirrored position as destination!");
-        self.destination = unitHelper.reflect(self.me, self.getPassableMap(), self.me.id % 2 === 0);
+        self.log("Adding mirrored spawn as destination!");
+        self.log(self.spawnPoint);
+        self.log(self.mirroredSpawn);
+        self.destination = self.mirroredSpawn;
         self.distanceMap = unitHelper.createDistanceMap(self.destination, self.map, self.getVisibleRobotMap());
       }
       else if(self.task === "guard_castle"){
@@ -58,9 +77,12 @@ var crusaderHelper = {
       // Walk towards enemies within view range
       for(var i = 0; i<nearbyRobots.length; i++){
         if(nearbyRobots[i].team != self.me.team){
+          let previousDestination = self.destination;
           self.destination = nearbyRobots[i];
           self.distanceMap = unitHelper.createDistanceMap(self.destination, self.map, self.getVisibleRobotMap());
           let nextDirection = unitHelper.getNextDirection(location, 2, self.distanceMap);
+          self.destination = previousDestination;
+          self.distanceMap = unitHelper.createDistanceMap(self.destination, self.map, self.getVisibleRobotMap());
           return self.move(nextDirection.x, nextDirection.y);
         }
       }
@@ -70,17 +92,22 @@ var crusaderHelper = {
       // If at destination and no enemy to attack or walk towards,
       // go to new guard position
       if(distanceToDestination<=2){
-        self.destination = unitHelper.getCastleGuardPosition(self.castle, self.map);
-        self.distanceMap = unitHelper.createDistanceMap(self.destination, self.map, self.getVisibleRobotMap());
+        return;
       }
     }else if(self.task === "attack_opponent"){
       // TODO: If there is a Message from other robot saying help, go there!
 
-      // If at destination and no enemy to attack or walk towards,
-      // go to random karbonite source just for fun
       if(distanceToDestination<=2){
-        self.log("Adding random karb as destination!");
-        self.destination = randomKarb;
+          self.log("Go back to spawn!");
+          self.task = "go_to_castle";
+          self.destination = self.spawnPoint;
+          self.distanceMap = unitHelper.createDistanceMap(self.destination, self.map, self.getVisibleRobotMap());
+      }
+    }else if(self.task === "go_to_castle"){
+      if(distanceToDestination<=2){
+        self.task = "attack_opponent";
+        self.log("Go back to mirroredSpawn!");
+        self.destination = self.mirroredSpawn;
         self.distanceMap = unitHelper.createDistanceMap(self.destination, self.map, self.getVisibleRobotMap());
       }
     }
