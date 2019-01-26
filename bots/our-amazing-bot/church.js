95423786b28a79bc9c1c902991e0aa3366ccead8
Joakim Loxdal <loxdal@kth.se>
2019-01-26 13:52:29 +0100

    Church now spawns pilgrim if none visible

--- bots/our-amazing-bot/church.js
+++ bots/our-amazing-bot/church.js
@@ -17,17 +17,20 @@ var churchHelper = {
     // }
     let location = {x: self.me.x, y: self.me.y};
 
-    const allies = self.getVisibleRobots().filter(r => r.team === self.me.team && r.unit === SPECS.PROPHET);
+    const visible_allies = self.getVisibleRobots().filter(r => r.team === self.me.team);
+    const visible_prophets = visible_allies.filter(r => r.unit === SPECS.PROPHET);
+    const visible_pilgrims = visible_allies.filter(r => r.unit === SPECS.PILGRIM);
     const enemies = self.getVisibleRobots().filter(r => r.team !== self.me.team);
 
     if (self.step === 1) self.spawnedProphets = 0;
+    if (self.step === 1) self.spawnedPilgrims = 0;
     if (self.step === 1) self.spawnDefense = 0;
 
     if(!enemies.length){
       self.spawnDefense = 0;
     }
 
-    if (enemies.length > 0 && self.spawnDefense < 7) {
+    if (enemies.length > 0 && self.spawnDefense < 7 && self.karbonite >= 25 && self.fuel >= 50) {
       let shortestDist = Infinity;
       let closestEnemy = enemies[0];
 
@@ -48,7 +51,28 @@ var churchHelper = {
       } else {
         self.log("No direction was found - cannot build");
       }
-    } else if ((self.karbonite >= 25 + self.SK && self.fuel >= 50 + self.SF && (self.spawnedProphets < ((self.step - self.step % 15) / 15 + 1))) || (self.karbonite > 250 && self.fuel > 500) || self.step > 700) {
+    } else if(!visible_pilgrims.length
+      && self.karbonite >= 25 + self.SK
+      && self.fuel >= 50 + self.SF
+      && self.spawnedPilgrims <= 5
+    ){
+
+      let possibleDirections = structureHelper.getPossibleDirections(location, self.map, self.getVisibleRobotMap())
+      let direction = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
+
+      if (direction) {
+        self.spawnedPilgrims++;
+        self.log('Church building a pilgrim at ' + (self.me.x + direction.x) + ',' + (self.me.y + direction.y));
+        return self.buildUnit(SPECS.PILGRIM, direction.x, direction.y);
+      } else {
+        self.log("No random direction was found - cannot build");
+      }
+
+    } else if ((self.karbonite > 25 + self.SK
+      && self.fuel > 50 + self.SF
+      && (self.spawnedProphets < ((self.step - self.step % 15) / 15 + 1)))
+    || (self.karbonite > 250 && self.fuel > 500)
+    || self.step > 700) {
 
       let possibleDirections = structureHelper.getPossibleDirections(location, self.map, self.getVisibleRobotMap())
       let direction = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
@@ -62,8 +86,6 @@ var churchHelper = {
       }
 
     }
-
-
 
     return null;
   }
